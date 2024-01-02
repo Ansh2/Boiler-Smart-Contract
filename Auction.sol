b40c95a9c2d7d1c41b3588ca258b7acf0819efdd
Ansh Tandon <ansh.nynj@gmail.com>
2024-01-02 12:03:12 -0500

    Update Auction.sol
    Created new auction that is able to have a set end time that uses uints intsead of uint265

--- Auction.sol
+++ Auction.sol
@@ -1,1 +1,27 @@
+/ SPDX-License-Identifier: MIT
 
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Auction {
+    bool public ended;
+    uint public maximum_bid; 
+    uint public end_time = 1698983613;
+    address public max_bidder; 
+    address public seller; 
+
+    // struct Person {
+    //     uint age;
+    //     uint first_name;
+    //     uint last_name;
+    
+    // }   
+
+    constructor(uint256 ending) {
+        ended = false;
+        seller = msg.sender;
+        end_time = ending;
+    }
+
+
+
+}
