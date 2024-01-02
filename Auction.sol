9e661f058656793b9e0f17ae99e022df87409037
Ansh Tandon <ansh.nynj@gmail.com>
2024-01-02 12:07:52 -0500

    Update Auction.sol
    added new. payable function that enables user to bid and settle bid, ensured proper conditions are met for the payment on the product.

--- Auction.sol
+++ Auction.sol
@@ -22,6 +22,42 @@ contract Auction {
         end_time = ending;
     }
 
+    //function bid public payable
+    // paramaters: no parameters. YOu can use msg.sender for the address
+    // anad msg.value for the amount
+    // return type: none
+
+     function bid() external payable {
+        //conditions to check if auction has ended
+        require(ended, "Highest bidder already given price");
+        require(getCurrentTime() >= end_time, "Auction time has ended");
+        if (msg.value > maximum_bid) {
+            maximum_bid = msg.value;
+            max_bidder = msg.sender;
+        }
+
+        // require(msg.value > maximum_bid, "Bid should be greater than max");
+ 
+    }
+
+    //revert if certain requirements aren't met
+
+    //pay back current highest bidder
+    // reset highest bidder highest bid; 
+    //To transfer: add.transfer(value)
+
+    function settle() public payable {
+        require(getCurrentTime() >= end_time, "Auction time has ended");
+        payable(max_bidder).transfer(maximum_bid);
+        
+    }
+
+    //function to view max bid
+
+    function getCurrentTime() public view returns(uint){
+        return block.timestamp;
+    }
+
 
 
 }
